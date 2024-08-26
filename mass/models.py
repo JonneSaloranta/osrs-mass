74a67ca1e60b9880fb20d984fcb40f568fcbee58
Jonne <saloranta.jonne@gmail.com>
2024-08-26 20:42:40 +0300

    updated mass app

--- mass/models.py
+++ mass/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+import time
+import uuid
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=1000)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, blank=True)
+    location = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    invite_code = models.CharField(max_length=6, unique=True, blank=True, null=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.invite_code:
+            self.invite_code = self.generate_invite_code()
+        super(Event, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return f'/details/{self.uuid}'
+    
+
+    def generate_invite_code(self):
+        import random
+        import string
+        return ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
--- mass/views.py
+++ mass/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .models import Event
+
+
+from .models import Event
+
+def index(request):
+    all_events = Event.objects.all()
+
+    context = {
+        'events': all_events
+    }
+    return render(request, 'mass/index.html', context=context)
+
+def create(request):
+    return render(request, 'mass/create_event.html')
+
+def update(request):
+    return render(request, 'mass/update_event.html')
+
+def delete(request):
+    return render(request, 'mass/index.html')
+
+def join(request):
+    return render(request, 'mass/index.html')
+
+def leave(request):
+    return render(request, 'mass/index.html')
+
+def details(request, id):
+    event = get_object_or_404(Event, pk=id)
+    context = {
+        'event': event
+    }
+    return render(request, 'mass/datails_event.html' , context=context)
+
