499d18db4406e795924e9a91dd8604303e1bdbf2
JonneSaloranta <saloranta.jonne@gmail.com>
2024-09-02 22:33:12 +0300

    added join path for joining with invite code

--- mass/urls.py
+++ mass/urls.py
@@ -1,13 +1,13 @@
 
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create, name='create'),
+    path('join/', views.join, name='join'), 
     path('update/<int:id>/', views.update, name='update'),
     path('delete/<int:id>/', views.delete, name='delete'),
-    path('join/<int:id>/', views.join, name='join'),
     path('leave/<int:id>/', views.leave, name='leave'),
     path('details/<int:id>/', views.details, name='details'),
 ]
