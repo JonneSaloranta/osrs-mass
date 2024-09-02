94ee406a6e3d160a5bd53729b35ede5c8ae8b18e
JonneSaloranta <saloranta.jonne@gmail.com>
2024-09-03 01:33:33 +0300

    added urls for custom login

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(('custom_login.urls', 'auth'), namespace='auth')),
     path('', include(('website.urls','website'),namespace='website')),
     path('event/', include(('mass.urls','mass'),namespace='mass')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
