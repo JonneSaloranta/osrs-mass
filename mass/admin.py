ed718a94ee50ef7d0647dafb1ddccdb1e2eac6fc
JonneSaloranta <saloranta.jonne@gmail.com>
2024-09-02 22:32:07 +0300

    updated admin page

--- mass/admin.py
+++ mass/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 
-from .models import Event
+from .models import Event, InviteUsage, Requirement
 
-admin.site.register(Event)
+class RequirementInline(admin.TabularInline):
+    model = Event.requirements.through
+    extra = 3
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_date', 'end_date', 'location')
+    search_fields = ('name', 'location', 'invite_code')
+    list_filter = ('start_date', 'end_date', 'location')
+    inlines = [RequirementInline]
+    # hide the requirements field in the admin
+    exclude = ('requirements','invite_code')
+
+admin.site.register(Event, EventAdmin)
+
+admin.site.register(InviteUsage)
+admin.site.register(Requirement)
