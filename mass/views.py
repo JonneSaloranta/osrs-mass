d3cc6e57cf3ebb9af95f7d27a8d9aab29d74415a
JonneSaloranta <saloranta.jonne@gmail.com>
2024-09-02 22:33:54 +0300

    added join view and fixed a typo

--- mass/views.py
+++ mass/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render 
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.utils import timezone
 
-from .models import Event
+from .models import Event, InviteUsage
 
 def index(request):
     all_events = Event.objects.all()
@@ -20,8 +21,47 @@ def update(request):
 def delete(request):
     return render(request, 'mass/index.html')
 
+from django.shortcuts import render
+
 def join(request):
-    return render(request, 'mass/index.html')
+    invite_code = request.GET.get('invite_code')
+    
+    if invite_code:
+        # Retrieve the event using the invite code
+        event = get_object_or_404(Event, invite_code=invite_code)
+
+        # Identify the user, either by session key or IP address
+        user_id = request.session.session_key or request.META.get('REMOTE_ADDR')
+
+        # Get or create an InviteUsage record for this invite code and user
+        invite_usage, created = InviteUsage.objects.get_or_create(invite_code=invite_code, user_id=user_id)
+
+        # Check if the invite code usage is still valid (within 24 hours)
+        if invite_usage.is_valid():
+            # Update the last used timestamp
+            invite_usage.last_used = timezone.now()
+            invite_usage.save()
+
+            # If valid, pass the event to the context
+            context = {
+                'event': event
+            }
+
+            # Render the event details page
+            return render(request, 'mass/details_event.html', context=context)
+        else:
+            # If the invite code is expired, show an error
+            context = {
+                'error': 'Your invite code has expired. Please request a new one.'
+            }
+    else:
+        # If no invite code is provided or it doesn't exist, show an error
+        context = {
+            'error': 'Invalid invite code'
+        }
+
+    # Render the index page with the error context
+    return render(request, 'mass/index.html', context=context)
 
 def leave(request):
     return render(request, 'mass/index.html')
@@ -31,5 +71,5 @@ def details(request, id):
     context = {
         'event': event
     }
-    return render(request, 'mass/datails_event.html' , context=context)
+    return render(request, 'mass/details_event.html' , context=context)
 
