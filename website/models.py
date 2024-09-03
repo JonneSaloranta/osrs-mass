03b443c8d0a3b2210f15cad6ae7dcefbbf250eb9
JonneSaloranta <saloranta.jonne@gmail.com>
2024-09-03 14:40:36 +0300

    added nickname model for users

--- website/models.py
+++ website/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from email_login.models import User
+
+class UserNickname(models.Model):
+    nickname = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nickname
