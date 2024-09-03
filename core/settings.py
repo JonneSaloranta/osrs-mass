317976fb7e4a82274b8238fe117dfa4a3f5d5213
JonneSaloranta <saloranta.jonne@gmail.com>
2024-09-03 12:44:41 +0300

    added context prosessor to access USE_EMAIL_ACTIVATION variable

--- core/settings.py
+++ core/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'custom_login.context_processors.use_email_activation',
             ],
         },
     },
